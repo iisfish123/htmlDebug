9329ea22f9194d05cfb192e111c16f0c31d3da43
likezheng <likezheng@fuse.co.id>
2022-07-18 15:59:41 +0800

    RP-0000 test

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/"+"index.html")        
+})
+
+app.get("/PlanBenefitItem.html",(req,res)=>{
+    res.sendFile(__dirname+"/"+"PlanBenefitItem.html")    
+})
+
+app.get("/imgs/*",(req,res)=>{
+    res.sendFile(__dirname+req.url)    
+})
+
+app.get("/css/*",(req,res)=>{
+    res.sendFile(__dirname+req.url)
+})
+
+app.get("/js/*",(req,res)=>{
+    res.sendFile(__dirname+req.url) 
+})
+
+app.use('/insurance-finance-vs-api/api/fuse/', createProxyMiddleware({ target: 'https://dev.fvsenano.com', changeOrigin: true,secure:false, }));
+app.use('/f4/api/scm/', createProxyMiddleware({ target: 'https://dev.fvsenano.com', changeOrigin: true,secure:false, }));
+
+app.listen(8000);
